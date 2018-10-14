f1c0ea74fab7e53aca957c4f6398b9831348c791
Mohammad Anwari <mdamt@mdamt.net>
2018-10-15 01:23:04 +0700

    Export all vars

--- deploy/settings.sh
+++ deploy/settings.sh
@@ -1,16 +1,25 @@
-DB_NAME=${DB_NAME:=ngobrel}
-DB_USER=${DB_USER:=dbuser}
-DB_PASS=${DB_PASS:=dbpass}
-DB_HOST=${DB_HOST:=localhost}
-DB_PORT=${DB_PORT:=5432}
+DB_NAME=${DB_NAME:-ngobrel}
+DB_USER=${DB_USER:-dbuser}
+DB_PASS=${DB_PASS:-dbpass}
+DB_HOST=${DB_HOST:-localhost}
+DB_PORT=${DB_PORT:-5432}
 DB_URL=postgres://$DB_USER:$DB_PASS@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=disable
 
-REDIS_HOST=${REDIS_HOST:=localhost}
-REDIS_PORT=${REDIS_PORT:=6379}
+REDIS_HOST=${REDIS_HOST:-localhost}
+REDIS_PORT=${REDIS_PORT:-6379}
 REDIS_URL="$REDIS_HOST:$REDIS_PORT"
 
-SMS_ACCOUNT=${SMS_ACCOUNT:=twilio-account-id}
-SMS_TOKEN=${SMS_TOKEN:=twilio-token}
+SMS_ACCOUNT=${SMS_ACCOUNT:-twilio-account-id}
+SMS_TOKEN=${SMS_TOKEN:-twilio-token}
+
+export DB_NAME
+export DB_USER
+export DB_PASS
+export DB_HOST
+export DB_PORT
+
+export REDIS_HOST
+export REDIS_PORT 
 
 export DB_URL
 export REDIS_URL
--- docker/tasks/ngobrel-server/ngobrel-server.sh
+++ docker/tasks/ngobrel-server/ngobrel-server.sh
@@ -4,7 +4,11 @@ if [ -f /conf/settings.sh ];then
     . /conf/settings.sh
 fi
 
+
+echo "Deploying DB...."
 cd /deploy
 ./deploy.sh
+
+echo "Running server..."
 
 /bin/ngobrel-server
